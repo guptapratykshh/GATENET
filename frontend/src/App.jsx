51bdc609d5226f1680b0e4cf29f57750599133af
Divyanshi <divyanshitalreja12@gmail.com>
2025-05-13 22:35:05 +0530

    Added the settings section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import Login from "./Login";
 import Signup from "./Signup";
+import Settings from "./Settings";
+// import User from "./User";
 const App = () => {
   return (
     <div>
-     {/* <Login/> */}
-     <Signup/>
+     <Login/>
+     {/* <Signup/> */}
+     {/* <Settings/> */}
+     {/* <User/> */}
     </div>
   );
 };
